cfa11dd8f95e708bc809b770fd67b165cb3e68c3
Karlen95h <karlen.h.1995@gmail.com>
2021-06-17 18:12:50 +0400

    Git Project

--- classes/GrassBuilder.js
+++ classes/GrassBuilder.js
@@ -1,5 +1,6 @@
-class GrassBuilder {
+class GrassBuilder extends LivingCreature {
     constructor(x, y) {
+        super(x, y)
         this.x = x;
         this.y = y;
         this.directions = [
@@ -17,23 +18,7 @@ class GrassBuilder {
             [this.x, this.y + 1],
         ];
     }
-    chooseCell(char) {
-        this.getNewCordinates();
-        let result = [];
-
-        for (let i = 0; i < this.directions.length; i++) {
-            let x = this.directions[i][0];
-            let y = this.directions[i][1];
-
-            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
-                if (matrix[y][x] == char) {
-                    result.push(this.directions[i]);
-                }
-            }
-
-        }
-        return result;
-    }
+   
     eat() {
         let found = this.chooseCell(0);
         let exact = random(found)
--- classes/GrassEater.js
+++ classes/GrassEater.js
@@ -1,5 +1,6 @@
-class GrassEater {
+class GrassEater extends LivingCreature {
     constructor(x, y) {
+        super(x , y)
         this.x = x;
         this.y = y;
         this.energy = 20;
@@ -26,24 +27,7 @@ class GrassEater {
             [this.x + 1, this.y + 1]
         ];
     }
-    chooseCell(char) {
-        this.getNewCordinates();
-        let result = [];
-
-        for (let i = 0; i < this.directions.length; i++) {
-            let x = this.directions[i][0];
-            let y = this.directions[i][1];
-
-            if (y < matrix.length && y >= 0 && x < matrix[0].length && x >= 0) {
-                if (matrix[y][x] == char) {
-                    result.push(this.directions[i]);
-                }
-            }
-
-        }
-
-        return result;
-    }
+    
     mul() {
         let found = this.chooseCell(0);
         let exact = random(found)
